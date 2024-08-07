56095cd8bffa17c39d1ba1d2e648d19ad8479792
TeoCos27 <teodorcostescu@gmail.com>
2024-08-07 23:59:40 +0300

    Create Scripts.meta

---
+++ Assets/Scripts/BaseBasicAbilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBasicAbilities : MonoBehaviour
+{
+    public float CooldownFirstAbility=0f;
+    public float CooldownSecondAbility=0f;
+    public virtual void CastFirstAbility(){
+        
+    }
+    public virtual void CastSecondAbility(){
+       
+    }
+}
