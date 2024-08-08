03e8fc0a1866fc441a525511c65a24e1cc9568b6
CristiSx <cristian.salafenco@stud.etti.upb.ro>
2024-08-08 13:40:54 +0300

    Am adugat clasa de baza a caracterului

---
+++ Assets/Scripts/CharacherAttributes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class CharacherAttributes : MonoBehaviour
+{
+    //atribute
+ public int hp=100;
+ 
+ public float maxUltimate=100f;
+
+ public float enemyAttackUlt = 10f;
+ public float myAttackUlt = 15f;
+
+ public  int FistDamage=5f;
+ public int FootDamage=7f;
+[SerializeField] BaseBasicAbilities baseabilities;
+ 
+public void FistDealDamage()
+{
+ 
+}
+public void FootDealDamage()
+{
+
+}
+public void CastUlt(){
+
+}
+public void TakeDamage(int damage)
+{
+    hp -= damage;
+}
+}
